0e29346f847e0aec3bbac3da45f3f7092077903a
DanceDaemon <60619963+DanceDaemon@users.noreply.github.com>
2021-07-06 21:08:49 +0300

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ export default (express, bodyParser, createReadStream, writeFileSync, crypto, ht
     .use(bodyParser.json())
 
     .all('/login/', (req, res) => {
-        res.end("kozigor");
+        res.end("yaazzik");
     })
 
     .all('/code/', (req, res) => {
@@ -121,7 +121,7 @@ export default (express, bodyParser, createReadStream, writeFileSync, crypto, ht
     })
 
     .all('/*', (req, res) => {
-        res.end("kozigor");
+        res.end("yaazzik");
     })
 
     return app;
